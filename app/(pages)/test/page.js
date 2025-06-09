75d57683b8c4d99d95f46253acc9306e3e906446
Kawara <39919017+kawaraa@users.noreply.github.com>
2025-06-09 13:10:55 +0200

    Finished testing push notification v-2.

--- app/(pages)/test/page.js
+++ app/(pages)/test/page.js
@@ -1,14 +1,19 @@
 "use client";
 
 import { urlBase64ToUint8Array } from "@/app/services/encodin-helper";
+import { useState } from "react";
 
 const key = process.env.NEXT_PUBLIC_VAPID_KEY;
 
 export default function Test() {
+  const { result, setResult } = useState("");
+
   const requestPushNotification = async () => {
     try {
       console.log("requestPushNotification");
+      setResult("requestPushNotification");
       const permission = await Notification.requestPermission();
+      setResult(JSON.stringify(permission, null, 2));
 
       if (permission === "granted") {
         const registration = await navigator.serviceWorker.ready;
@@ -16,24 +21,29 @@ export default function Test() {
           userVisibleOnly: true,
           applicationServerKey: urlBase64ToUint8Array(key), // From `web-push generate-vapid-keys`
         });
+        setResult(JSON.stringify(subscription, null, 2));
 
-        console.log("Push subscription:", subscription.keys);
+        console.log("Push subscription:", subscription);
 
         // Send subscription to your backend (for testing, log it)
-        await fetch("/api", {
+        const res = await fetch("/api", {
           method: "POST",
           body: JSON.stringify(subscription),
           headers: { "Content-Type": "application/json" },
         });
       }
+      setResult(JSON.stringify(res, null, 2));
     } catch (error) {
       console.log("requestPushNotification: ", error);
+      setResult(JSON.stringify(error, null, 2));
     }
   };
 
   return (
     <div className="">
       <button onClick={requestPushNotification}>Enable Notifications</button>
+
+      <pre>{result}</pre>
     </div>
   );
 }
